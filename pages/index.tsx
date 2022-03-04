ad562a87155338701dba6487b7bb44da66cc9a89
awaisspk <iamawaisiqball@gmail.com>
2022-03-04 14:41:11 +0500

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,41 @@
+import { About } from "@components/about";
+import Image from "next/image";
+import { Feedback } from "@components/feedback";
+import { Footer } from "@components/footer";
+import { Header } from "@components/header";
+import { HeroSection } from "@components/heroSection";
+import { PrayerCards } from "@components/prayerCard";
+import { PrayerPreparation } from "@components/prayerPrepration";
+import { PageLayout } from "@src/layouts";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <PageLayout>
+      <div className="">
+        <main className="max-w-screen">
+          <div className="w-full  relative h-screen  bg-cover bg-bottom md:bg-top bg-no-repeat">
+            <div>
+              <Image
+                layout="fill"
+                className="object-center object-cover pointer-events-none"
+                src="/img/newhero-bg.jpg"
+                alt=""
+              />
+              <div className="px-5 bg-black/70 min-h-screen relative z-1">
+                <Header />
+                <HeroSection />
+              </div>
+            </div>
+          </div>
+          <PrayerCards />
+          <PrayerPreparation />
+          <About />
+          <Feedback />
+        </main>
+        <Footer />
+      </div>
+    </PageLayout>
   );
 };
 
