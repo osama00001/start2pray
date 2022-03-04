e9ea361092433658f1bb8abcf9754ac6dd9b67f7
awaisspk <iamawaisiqball@gmail.com>
2022-03-04 14:41:04 +0500

    chores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  mode : "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        primary: "#29323c",
+        secondary: "#6f91a7",
+      },
+      gridTemplateRows: {
+        layout: "max-content 1fr max-content",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/img/newhero-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
