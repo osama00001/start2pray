73e69a55880adc6f05c62ca3424fac49abbf4914
awaisspk <iamawaisiqball@gmail.com>
2022-03-04 14:39:34 +0500

    hero section

---
+++ src/components/heroSection/index.tsx
@@ -0,0 +1,17 @@
+export const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="max-w-[1000px]  ml-5 sm:ml-10 pt-52 lg:mx-auto space-y-10"
+    >
+      <h1 className="space-y-2  text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-bold">
+        <div>Ibadi Online</div>
+        <div>Prayer platform</div>
+        <div className="text-[#0D9BA3]">-made in OMAN-</div>
+      </h1>
+      <div className="ml-3 outline cursor-pointer select-none uppercase md:text-2xl outline-offset-[10px] w-max outline-white/90 text-white/90 hover:outline-white/70 active:outline-white/80 outline-2 py-1 px-10">
+        <a>let&apos;s pray</a>
+      </div>
+    </section>
+  );
+};
